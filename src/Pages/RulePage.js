f6b8a5abab05eba08c18c2eceaa9ee9fb548e7b0
Tom Piercy <tap32@medschl.cam.ac.uk>
2021-03-02 15:17:16 +0000

    Colour references to colours in the instruction text

--- src/Pages/RulePage.js
+++ src/Pages/RulePage.js
@@ -4,15 +4,26 @@ import { connect } from 'react-redux';
 import { goToNextTaskState } from '../redux/taskactions';
 import { getCurrentRuleText } from '../redux/selectors';
 
+import {
+	HUMAN_READABLE_COLOURS,
+} from '../constants';
+
+
+const colourRe = new RegExp(`(${Object.values(HUMAN_READABLE_COLOURS).join('|')})`, 'g');
+
+
 class RulePage extends React.Component{
 	componentDidUpdate() {
 		window.scrollTo(0, 0);
 	}
 
 	render() {
+		const text = (<p>
+			{this.props.ruleText.split(colourRe).map((t,i) => t.match(colourRe)? <span key={i} className={t}>{t}</span>:<span key={i}>{t}</span>)}
+		</p>);
 		return (
 			<div>
-				<p>{this.props.ruleText}</p>
+				{text}
 				<button onClick={this.props.nextPage} className="ContinueButton">Continue</button>
 			</div>
 		);
